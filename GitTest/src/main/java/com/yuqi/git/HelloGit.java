49ea2e07256ea99048f1a6e2420cbc5e19a98bd2
yuqi <864619545@qq.com>
2020-10-30 21:18:04 +0800

    创建aa分支

--- GitTest/src/main/java/com/yuqi/git/HelloGit.java
+++ GitTest/src/main/java/com/yuqi/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("更新一");
         System.out.println("创建分支");
+        System.out.println("创建啊啊分支");
     }
 }
